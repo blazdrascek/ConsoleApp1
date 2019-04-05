2e142a0438f5cfdf717610bbde7e00e8f407bebf
Erik Maček <erikmacek@gmail.com>
2019-04-05 11:10:44 +0200

    komentar

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Pozdravljen git");
+            Console.ReadLine();
+            //
         }
     }
 }
